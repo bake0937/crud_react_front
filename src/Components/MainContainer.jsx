b34734582160fa640ff30c241a07d9ea6a8fe83a
bake0937 <sadayasu.info@gmail.com>
2018-04-04 08:42:57 +0900

    つぶやきを投稿できるようにする

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -4,6 +4,7 @@ import ProductsContainer from './ProductsContainer'
 import FormContainer from './FormContainer'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import update from 'react-addons-update';
 
 class MainContainer extends React.Component {
   constructor(props) {
@@ -24,10 +25,21 @@ class MainContainer extends React.Component {
       })
   }
 
+  createProduct = (product) => {
+    axios.post('http://localhost:3001/products', { product: product })
+      .then((response) => {
+        const newData = update(this.state.products, { $push: [response.data] })
+        this.setState({ products: newData })
+      })
+      .catch((data) => {
+        console.log(data)
+      })
+  }
+
   render() {
     return (
       <div className='app-main'>
-        <FormContainer />
+        <FormContainer createProduct={this.createProduct} />
         <ProductsContainer productData={this.state.products} />
       </div>
     );
